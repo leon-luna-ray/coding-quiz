9d3a27144a178f3e717b56688c5fca2209789150
Ray Luna <leon.luna.ray@gmail.com>
2021-10-16 19:20:15 -0700

    api bug fix loading screen functional

--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -1,10 +1,10 @@
 import React from 'react';
-const AnswerButton = ({ index, choice, handleAnswer }) => {
+const AnswerButton = ({ index, choice, onClick }) => {
   return (
     <div
       className='choice-btn'
       onClick={() => {
-        handleAnswer(choice);
+        onClick(choice);
       }}
     >
       <span className='choice-prefix'>{index + 1}. </span>
--- src/components/Game.js
+++ src/components/Game.js
@@ -7,35 +7,44 @@ import GameOver from './GameOver';
 import testQuestions from './questions';
 
 const Game = () => {
+  const userScore = parseInt(localStorage.getItem('coding-quiz-score'));
+  // State
+  const [loading, setLoading] = useState(true);
   const [score, setScore] = useState(0);
   const [questions, setQuestions] = useState(null);
   const [questionIndex, setQuestionIndex] = useState(0);
-  // Current question and score from state
-  const userScore = parseInt(localStorage.getItem('coding-quiz-score'));
-  const question = testQuestions[questionIndex].question;
-  const choices = testQuestions[questionIndex].choices;
-  const answer = testQuestions[questionIndex].answer;
-
   // Avoid resetting score on reload
   useEffect(() => {
     if (userScore) {
       setScore(userScore);
     }
-
     const fetchQuestions = async () => {
       const questions = await axios.get(
         'https://quizapi.io/api/v1/questions?apiKey=gpVMgV0KdzCQI3oQBr4a8EGNpc0HnJqPcK2MvVYu&category=code&limit=20'
       );
       setQuestions(questions.data);
+      setLoading(false);
       console.log(questions.data);
     };
     fetchQuestions();
   }, []);
-
   // Update local storage on score state change
   useEffect(() => {
     localStorage.setItem('coding-quiz-score', score);
   }, [score]);
+
+  // Handle Loading
+  if (loading) {
+    return <h1>Start!</h1>;
+  }
+  // Handle game over
+  if (questionIndex === testQuestions.length - 1) {
+    return <GameOver score={score} />;
+  }
+  // Game
+  const question = questions[questionIndex].question;
+  const choices = testQuestions[questionIndex].choices;
+  const answer = testQuestions[questionIndex].answer;
 
   const handleAnswer = (userChoice) => {
     // add additional logic here to show which one is the correct one
@@ -45,14 +54,6 @@ const Game = () => {
     }
     setQuestionIndex(questionIndex + 1);
   };
-  // Handle Loading
-  if (questions === null) {
-    return <h1>Start!</h1>;
-  }
-  // Handle game over
-  if (questionIndex === testQuestions.length - 1) {
-    return <GameOver score={score} />;
-  }
 
   return (
     <section className='game'>
