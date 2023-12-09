4a5eff47b995cd161ed6c5cfa86275bf7b327537
Ray Luna <leon.luna.ray@gmail.com>
2023-12-09 14:02:11 -0800

    rm unused code

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,19 +5,12 @@ const BASE_URL = import.meta.env.VITE_BASE_API_URL;
 
 export const fetchQuiz = async (limit, category, difficulty, tags) => {
     const query = `${BASE_URL}/questions?&category=${category}&difficulty=${difficulty}&limit=20&tags=${tags}`;
-
     try {
         const response = await axios.get(query, {
-            headers: {
-                'X-Api-Key': API_TOKEN,
-            },
-            params: {
-                limit: limit,
-            },
+            headers: { 'X-Api-Key': API_TOKEN, },
+            params: { limit: limit, },
         });
-
         return response.data;
-
     } catch (error) {
         console.error(error);
     }
