d6ba47f0ac3386e6edcb7032ddae949d128db4b1
Ray Luna <leon.luna.ray@gmail.com>
2023-12-10 00:44:25 -0800

    add score to final screen

--- src/components/ScorePanel.jsx
+++ src/components/ScorePanel.jsx
@@ -5,9 +5,9 @@ const ScorePanel = () => {
   const { score } = useGameContext();
   
   return (
-    <div id='game' className='justify-center flex-column'>
+    <div id='game' className='justify-center flex-column pb-[2rem]'>
       <div className='hud-item'>
-        <p className='hud-prefix'>Score</p>
+        <p className='hud-prefix h3 uppercase'>Score</p>
         <h1 className='hud-main-text' id='score'>{score}</h1>
       </div>
     </div>
