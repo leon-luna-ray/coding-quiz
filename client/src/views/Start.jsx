56e7db15d858f56e2c9a94b7242ddcc45d511c98
Ray Luna <leon.luna.ray@gmail.com>
2025-08-20 22:27:05 -0700

    add languages

--- client/src/views/Start.jsx
+++ client/src/views/Start.jsx
@@ -9,21 +9,21 @@ const Start = () => {
           <h1>Coding Quiz!</h1>
         </div>
         <div className='flex-col-1 w-full grid grid-cols-3 gap-4'>
-          <Link className='btn tertiary bg-[#555555]' to='/quiz/c'>C</Link>
-          <Link className='btn secondary bg-[#178600]' to='/quiz/csharp'>C#</Link>
-          <Link className='btn bg-[#f34b7d]' to='/quiz/cpp'>C++</Link>
-          <Link className='btn bg-[#563d7c]' to='/quiz/css'>CSS</Link>
-          <Link className='btn bg-[#6e4a7e]' to='/quiz/elixir'>Elixir</Link>
-          <Link className='btn bg-[#00ADD8]' to='/quiz/go'>Go</Link>
-          <Link className='btn tertiary bg-[#e34c26]' to='/quiz/html'>HTML</Link>
-          <Link className='btn bg-[#b07219]' to='/quiz/java'>Java</Link>
-          <Link className='btn secondary bg-[#f1e05a]' to='/quiz/javascript'>JavaScript</Link>
-          <Link className='btn bg-[#4F5D95]' to='/quiz/php'>PHP</Link>
-          <Link className='btn bg-[#3572A5]' to='/quiz/python'>Python</Link>
-          <Link className='btn bg-[#dea584]' to='/quiz/rust'>Rust</Link>
-          <Link className='btn bg-[#e38c00]' to='/quiz/sql'>SQL</Link>
-          <Link className='btn bg-[#F05138]' to='/quiz/swift'>Swift</Link>
-          <Link className='btn bg-[#3178c6]' to='/quiz/typescript'>TypeScript</Link>
+          <Link className='btn tertiary bg-c' to='/quiz/c'>C</Link>
+          <Link className='btn secondary bg-csharp' to='/quiz/csharp'>C#</Link>
+          <Link className='btn bg-cpp' to='/quiz/cpp'>C++</Link>
+          <Link className='btn bg-css' to='/quiz/css'>CSS</Link>
+          <Link className='btn bg-elixir' to='/quiz/elixir'>Elixir</Link>
+          <Link className='btn bg-go' to='/quiz/go'>Go</Link>
+          <Link className='btn tertiary bg-html' to='/quiz/html'>HTML</Link>
+          <Link className='btn bg-java' to='/quiz/java'>Java</Link>
+          <Link className='btn secondary bg-javascript' to='/quiz/javascript'>JavaScript</Link>
+          <Link className='btn bg-php' to='/quiz/php'>PHP</Link>
+          <Link className='btn bg-python' to='/quiz/python'>Python</Link>
+          <Link className='btn bg-rust' to='/quiz/rust'>Rust</Link>
+          <Link className='btn bg-sql' to='/quiz/sql'>SQL</Link>
+          <Link className='btn bg-swift' to='/quiz/swift'>Swift</Link>
+          <Link className='btn bg-typescript' to='/quiz/typescript'>TypeScript</Link>
         </div>
       </div>
     </div>
