036f0f925a9e9eb5b1d3274b6136c58399e219d7
Ray Luna <leon.luna.ray@gmail.com>
2025-08-21 00:12:33 -0700

    bug fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { GameProvider, useGameContext } from '@/contexts/GameContext';
+import { useGameContext } from '@/contexts/GameContext';
 
-const HeaderContent = () => {
+const Header = () => {
     const location = useLocation();
-    const { score } = useGameContext();
+    let score = 0;
+    try {
+        if (location.pathname !== '/') {
+            const gameContext = useGameContext();
+            score = gameContext.score;
+        }
+    } catch (error) {
+        console.error("Error accessing game context:", error);
+    }
 
     return (
         <header className={location.pathname === '/' ? 'hidden' : ''}>
             <div className="container flex items-center justify-between py-[1rem] text-[2rem] text-white font-syne">
                 <Link to='/' className="hover:no-underline">Coding Quiz!</Link>
-                <p className=''>Score: {score}</p>
+                {/* <p className=''>Score: {score}</p> */}
             </div>
         </header>
     )
 }
 
-const Header = () => {
-    return (
-        <GameProvider>
-            <HeaderContent />
-        </GameProvider>
-    )
-}
-
 export default Header;
-
