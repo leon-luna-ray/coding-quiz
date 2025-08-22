1e16360c4f65c27fb21754bb29d16f1472742d7f
Ray Luna <leon.luna.ray@gmail.com>
2025-08-21 23:51:46 -0700

    add game flow styles

--- client/src/components/AnswerButton.jsx
+++ client/src/components/AnswerButton.jsx
@@ -3,9 +3,14 @@ import { useGameContext } from '@/contexts/GameContext';
 import { capitalizeLastLetter } from '@/lib/text';
 
 const AnswerButton = ({ letter, text, index }) => {
-  const { handleAnswerSubmit, quizType, isCorrect } = useGameContext();
+  const { handleAnswerSubmit, quizType, isCorrect, currentQuestion } = useGameContext();
+  const isThisCorrect = isCorrect !== null && currentQuestion?.correct_answers?.[`${letter}_correct`] === "true";
+  
   return (
-    <button className={`w-full btn flex gap-4 justify-center ${quizType.slug} ${isCorrect !== null ? 'disabled' : ''}`} onClick={() => { handleAnswerSubmit(letter); }}>
+    <button 
+      className={`w-full btn flex gap-4 justify-center ${quizType.slug} ${isCorrect !== null ? 'disabled' : ''} ${isThisCorrect ? 'correct' : ''}`} 
+      onClick={() => { handleAnswerSubmit(letter); }}
+    >
       <p className='font-[500]'>{capitalizeLastLetter(letter)}:</p>
       <p className='font-jetbrains'>{text}</p>
     </button>
