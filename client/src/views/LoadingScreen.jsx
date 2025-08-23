ee430f34803c081f5b6c8324926527085fe56eaf
Ray Luna <leon.luna.ray@gmail.com>
2025-08-23 00:43:39 -0700

    loading screen

--- client/src/views/LoadingScreen.jsx
+++ client/src/views/LoadingScreen.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import IconCode from '@/components/icons/IconCode'
+import React from 'react';
+import { useGameContext } from '@/contexts/GameContext';
+import { LANGUAGE_ICONS } from '@/lib/languageIcons';
 
 const LoadingScreen = () => {
-    return (
-        <div className='mx-auto flex justify-center items-center h-screen w-screen top-0 left-0 fixed flex-col-4'>
-            <div className="flex justify-center w-full max-w-[300px] animate-pulse">
-                <IconCode />
-            </div>
-            <h1 className='h3 uppercase'>Loading</h1>
-        </div>
-    )
-}
+  const { quizType } = useGameContext();
+  const icon = LANGUAGE_ICONS[quizType.slug] || '❓';
 
-export default LoadingScreen
+  return (
+    <div className='mx-auto flex justify-center items-center h-screen w-screen top-0 left-0 fixed flex-col-4'>
+      <div className="flex justify-center w-full max-w-[300px] animate-pulse">
+        <span
+          style={{ fontSize: '4rem', fontFamily: 'inherit' }}
+          className="language-icon text-white"
+        >
+          {icon}
+        </span>
+      </div>
+      <h1 className='h3 uppercase'>Loading</h1>
+    </div>
+  );
+};
+
+export default LoadingScreen;
